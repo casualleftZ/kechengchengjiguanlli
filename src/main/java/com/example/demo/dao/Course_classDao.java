f767633601bb1f3fcbd3cd4d05a9fb19719d9d9c
zhangxunice <2462832901@qq.com>
2019-06-23 02:07:12 +0800

    老师所带课程

--- src/main/java/com/example/demo/dao/Course_classDao.java
+++ src/main/java/com/example/demo/dao/Course_classDao.java
@@ -1,4 +1,11 @@
 package com.example.demo.dao;
 
+import com.example.demo.entity.Course_class;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
 public interface Course_classDao {
+    //根据教师ID查询教师的课
+    List<Course_class> getclass(Integer teacher_id);
 }
